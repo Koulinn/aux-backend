d98389cf7588dc539f4efdba4e89935db66df79b
Rafael Lima <drdverzola@gmail.com>
2021-12-23 07:01:19 +0000

    db tests

---
+++ __test__/__db_tests__/index.test.js
@@ -0,0 +1,15 @@
+import db_connect from '../../src/DB/db_connection.js'
+import client from '../../src/DB/db_config.js'
+
+describe('Testing the Database testing environment', () => {
+  beforeAll(async () => {
+    await db_connect()
+  })
+  it('should test that true is true', () => {
+    console.log('Test Database is running')
+    expect(true).toBe(true)
+  })
+  afterAll(async () => {
+    await client.end()
+  })
+})
