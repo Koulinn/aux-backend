6db6959aad87d58623c60dc616820dadb26acde7
Rafael Lima <drdverzola@gmail.com>
2021-12-23 07:03:26 +0000

    added APP_NODE and NODE_ENV

--- src/config/globalVariables/index.js
+++ src/config/globalVariables/index.js
@@ -1,6 +1,12 @@
-const { PORT } = process.env
+import dotenv from 'dotenv'
+
+if (process.env.NODE_ENV === 'test') dotenv.config()
+
+const { PORT, APP_MODE, NODE_ENV } = process.env
 
 const globalVariables = {
-    PORT,
+  PORT,
+  APP_MODE,
+  NODE_ENV,
 }
 export default globalVariables
