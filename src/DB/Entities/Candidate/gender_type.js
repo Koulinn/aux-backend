3e3669bb947bdec0e8f1c1b786708cf1b9b674da
Rafael Lima <drdverzola@gmail.com>
2021-12-24 11:19:58 +0000

    gender_type table

---
+++ src/DB/Entities/Candidate/gender_type.js
@@ -0,0 +1,8 @@
+const gender_type = `
+CREATE TABLE IF NOT EXISTS "gender_type"
+		_id" SERIAL,
+	    "gender_type" VARCHAR(100) NOT NULL,
+	    PRIMARY KEY ("_id")
+    );
+`
+export default gender_type
