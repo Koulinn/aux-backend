78f90a35e91b39b41d65175f4dc9c74084e80d17
Rafael Lima <drdverzola@gmail.com>
2022-01-12 20:54:01 +0000

    created job adverts

---
+++ src/DB/Tables/Main_entities/Job_advert/Job_adverts.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS "job_adverts" (
+	"job_advert_id" uuid DEFAULT uuid_generate_v4 (),
+	"exp_date" DATE NOT NULL,
+	"description" TEXT NOT NULL,
+	"salary_min" INTEGER NOT NULL,
+	"salary_max" INTEGER NOT NULL,
+	"city" VARCHAR(40),
+	"state" VARCHAR(2),
+	"recruiter_id" uuid REFERENCES users("acc_id") NOT NULL,
+	"company_id" uuid REFERENCES companies("company_id"),
+	PRIMARY KEY ("job_advert_id", "recruiter_id")
+);
