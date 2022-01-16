5e265d88e85247abdc77efbb6ba14577bd8f9c81
Rafael Lima <drdverzola@gmail.com>
2022-01-16 18:37:32 +0000

    added tests to prevent SQL injection from req.body

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,22 @@
-describe('Testing the testing environment', () => {
-  it('should test that true is true', () => {
-    console.log('Test is running')
-    expect(true).toBe(true)
+import testLib from './test_lib/index.js'
+
+const {
+  testingHandlers: { testSQLInjection },
+} = testLib
+
+describe('Testing SQL body injection prevention', () => {
+  it('should test that accept valid values', () => {
+    const body = {
+      email: 'test@mail.com',
+      password: '123456',
+    }
+    testSQLInjection(body, true)
+  })
+  it('should test that prevent invalid ;', () => {
+    const body = {
+      email: 'test@mail.com; SELECT * FROM USERS ;',
+      password: '123456',
+    }
+    testSQLInjection(body, false)
   })
 })
