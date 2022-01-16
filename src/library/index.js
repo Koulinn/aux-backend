1567c79bdd9f371d549a2efb7252f6d11b3b9a89
Rafael Lima <drdverzola@gmail.com>
2022-01-16 18:48:34 +0000

    imported middleware in the lib

--- src/library/index.js
+++ src/library/index.js
@@ -1,9 +1,11 @@
 import error_handlers from './error_handlers.js'
 import utils from './utils.js'
+import middleWares from './middlewares.js'
 
 const lib = {
   error_handlers,
   utils,
+  middleWares,
 }
 
 export default lib
