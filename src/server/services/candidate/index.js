9ba40ed5b0a7a296411717d161abc90d7b7e442b
Rafael Lima <drdverzola@gmail.com>
2022-01-16 18:43:23 +0000

    added bodySQLPrevention middleware to the create candidate route

--- src/server/services/candidate/index.js
+++ src/server/services/candidate/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import candidateHandlers from './candidate_handlers.js'
+import lib from '../../../library/index.js'
+
+const {
+  middleWares: { bodySQLPrevention },
+} = lib
 
 const { create } = candidateHandlers
 
 const router = express.Router()
 
-router.route('/').post(create)
+router.route('/').post(bodySQLPrevention, create)
 
 export default router
