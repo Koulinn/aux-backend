066b069df42c6f532f876fae1f537f37591eb6f4
Rafael Lima <drdverzola@gmail.com>
2022-01-16 09:59:49 +0000

    error handlers middlewares

--- src/library/error_handlers.js
+++ src/library/error_handlers.js
@@ -1,3 +1,64 @@
-const error_handlers = {}
+const notFound = (err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({
+      success: false,
+      msg: err.msg,
+    })
+  } else {
+    next(err)
+  }
+}
 
-export default error_handlers
+const badRequest = (err, req, res, next) => {
+  if (err.status === 400) {
+    res.status(400).send({
+      success: false,
+      msg: err.msg,
+    })
+  } else {
+    next(err)
+  }
+}
+
+const unauthorized = (err, req, res, next) => {
+  if (err.status === 401) {
+    console.log(err)
+    res.status(401).send({
+      status: false,
+      message: err.msg,
+    })
+  } else {
+    next(err)
+  }
+}
+
+const forbidden = (err, req, res, next) => {
+  if (err.status === 403) {
+    res.status(400).send({
+      success: false,
+      msg: err.msg,
+    })
+  } else {
+    next(err)
+  }
+}
+
+const serverError = (err, req, res, next) => {
+  console.log(err)
+
+  res.status(500).send({
+    success: false,
+    msg: err.msg || 'Server error',
+  })
+}
+
+// !important serverError MUST be the last
+const errorHandlers = [
+  notFound,
+  badRequest,
+  unauthorized,
+  forbidden,
+  serverError,
+]
+
+export default errorHandlers
