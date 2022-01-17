ca52d57ec4139ce6081eb2967b9cbf9a79c3acd0
Rafael Lima <drdverzola@gmail.com>
2022-01-17 15:09:09 +0000

    added constrains not null to account_type and UNIQUE to refreshToken

--- src/DB/Tables/Base_tables/Account_types/Account_types.sql
+++ src/DB/Tables/Base_tables/Account_types/Account_types.sql
@@ -2,3 +2,12 @@ CREATE TABLE IF NOT EXISTS "account_types" (
     "account_type" VARCHAR(40),
     PRIMARY KEY ("account_type")
 );
+
+INSERT INTO
+    account_types(account_type)
+VALUES
+    ('admin'),
+    ('candidate'),
+    ('recruiter'),
+    ('company'),
+    ('education_provider') ON CONFLICT DO NOTHING;
--- src/DB/Tables/Main_entities/Accounts/Accounts.sql
+++ src/DB/Tables/Main_entities/Accounts/Accounts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "accounts" (
     "acc_id" uuid DEFAULT uuid_generate_v4 (),
-    "account_type" VARCHAR(40) REFERENCES account_types("account_type"),
+    "account_type" VARCHAR(40) REFERENCES account_types("account_type") NOT NULL,
     "password" VARCHAR(80),
     "accepted_terms" BOOL NOT NULL,
     "email_primary" VARCHAR(80) UNIQUE NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS "accounts" (
     "github_id" VARCHAR(80) UNIQUE,
     "phone_number_primary" VARCHAR(20) UNIQUE,
     "phone_number_secondary" VARCHAR(20),
-    "refresh_token" VARCHAR(255),
+    "refresh_token" VARCHAR(255) UNIQUE,
     "CEP" VARCHAR(16),
     "state" CHAR(2),
     "city" VARCHAR(80),
