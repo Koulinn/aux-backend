1006713e6422851c2fe3519c6d11c7c9545546be
Rafael Lima <drdverzola@gmail.com>
2022-01-17 11:53:19 +0000

    create db_tests index test file

--- __test__/__db_tests__/index.test.js
+++ __test__/__db_tests__/index.test.js
@@ -1,15 +1,29 @@
 import db_connect from '../../src/DB/db_connection.js'
 import sequelize from '../../src/DB/db_config.js'
+import DBTestsHandlers from './DBTestsHandlers.js'
+import createTables from '../../src/DB/db_createTables.js'
+
+const { testCreateAccountWithEmailAndPassword } = DBTestsHandlers
 
 describe('Testing the Database testing environment', () => {
   beforeAll(async () => {
+    await createTables()
     await db_connect()
   })
   it('should test that true is true', () => {
     console.log('Test Database is running')
     expect(true).toBe(true)
   })
+
+  it('should test create user with email and password', () => {
+    const body = {
+      email_primary: 'test@mail.com',
+      password: '123456',
+    }
+    testCreateAccountWithEmailAndPassword(body)
+  })
   afterAll(async () => {
+    await sequelize.query('DROP SCHEMA public CASCADE; CREATE SCHEMA public;')
     await sequelize.close()
   })
 })
