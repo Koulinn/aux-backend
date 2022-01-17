4c9757c0b0aed51325e0a35271721fa9b79edb11
Rafael Lima <drdverzola@gmail.com>
2022-01-17 15:18:55 +0000

    swaped next and createError order to prevent server error

--- src/library/middleWares.js
+++ src/library/middleWares.js
@@ -5,8 +5,8 @@ const { createError, bodyHasSQLInjection } = utils
 const bodySQLPrevention = (req, res, next) => {
   const isInvalid = bodyHasSQLInjection(req.body)
   if (isInvalid) {
-    next()
     createError(403, 'Body invalid')
+    next()
   } else {
     next()
   }
