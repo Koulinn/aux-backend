8cbbdf7f3318b36fce7dc108d43d42367da1ee68
Rafael Lima <drdverzola@gmail.com>
2022-01-17 11:56:18 +0000

    added await and put them inside condition

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,8 +1,7 @@
 import db_connect from './db_connection.js'
 import createTables from './db_createTables.js'
 
-createTables()
-
 if (process.env.NODE_ENV !== 'test') {
-  db_connect()
+  await createTables()
+  await db_connect()
 }
