89ba544c97da1ee8380a0e56ebed349995f806e1
Rafael Lima <drdverzola@gmail.com>
2022-01-17 15:09:40 +0000

    added account_type to the create account

--- src/server/services/account/query_handlers.js
+++ src/server/services/account/query_handlers.js
@@ -5,9 +5,22 @@ const {
 } = authentication
 
 const createAccountWithEmailAndPasswordQuery = async (body) => {
-  const { email_primary, password, accepted_terms } = body
+  const { email_primary, password, accepted_terms, account_type } = body
   const hashedPassword = await hashPassword(password)
-  return `INSERT INTO accounts (email_primary, password, accepted_terms) VALUES ('${email_primary}', '${hashedPassword}', ${accepted_terms}) RETURNING acc_id;`
+  return `
+        INSERT INTO accounts (
+            email_primary, 
+            password, 
+            accepted_terms, 
+            account_type
+        ) VALUES (
+            '${email_primary}', 
+            '${hashedPassword}', 
+            ${accepted_terms}, 
+            '${account_type}') 
+        RETURNING 
+             acc_id
+      ;`
 }
 
 const query_handlers = {
