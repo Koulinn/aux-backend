14c610b78831d5a502dfd9d6692730b99402c59d
Rafael Lima <drdverzola@gmail.com>
2022-01-21 14:58:10 +0000

    added  google strategy to account creation

--- src/server/services/account/index.js
+++ src/server/services/account/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import accountHandlers from './account_handlers.js'
 import lib from '../../../library/index.js'
+import accountHandlers from './account_handlers.js'
+import passport from 'passport'
 
 const {
   middleWares: { bodySQLPrevention },
@@ -11,5 +12,15 @@ const { create } = accountHandlers
 const router = express.Router()
 
 router.route('/').post(bodySQLPrevention, create)
+router.route('/OAuth/google').get(
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+)
+router.route('/OAuth/redirect').get((req, res, next) => {
+  // find user, put token and redirect
+  res.cookie('cake_cookie', 'testValue')
+  res.redirect('http://localhost:3000/')
+})
 
 export default router
