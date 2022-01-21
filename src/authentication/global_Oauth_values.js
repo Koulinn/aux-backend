ce0871562edc18c39b9dae53b174b1ba3126abb4
Rafael Lima <drdverzola@gmail.com>
2022-01-21 14:56:23 +0000

    added global oAuth values

---
+++ src/authentication/global_Oauth_values.js
@@ -0,0 +1,12 @@
+const {
+  GOOGLE_OAUTH_CLIENT_ID,
+  GOOGLE_OAUTH_CLIENT_SECRET,
+  URL_REDIRECT_OAUTH,
+} = process.env
+
+const globalOAuthValues = {
+  GOOGLE_OAUTH_CLIENT_ID,
+  GOOGLE_OAUTH_CLIENT_SECRET,
+  URL_REDIRECT_OAUTH,
+}
+export default globalOAuthValues
