fcce12066dec16b90b87c3b292cec421cfd666c9
Rafael Lima <drdverzola@gmail.com>
2022-01-21 14:56:09 +0000

    added isExistentOAuth account, createGoogleAccount function and updated validateUser to validatePassword

--- src/authentication/auth_utils.js
+++ src/authentication/auth_utils.js
@@ -1,4 +1,12 @@
 import bcrypt from 'bcrypt'
+import OAuthQueryHandlers from './queryHandlers.js'
+import lib from '../library/index.js'
+
+const {
+  utils: { readQuery },
+} = lib
+const { createAccountWithOAuthQuery, searchOAuthAccountQuery } =
+  OAuthQueryHandlers
 
 const { hash, compare } = bcrypt
 
@@ -11,7 +19,7 @@ const hashPassword = async (password) => {
   }
 }
 
-const validateUser = async (password) => {
+const validatePassword = async (password) => {
   // const isCorrectPassword = await compare(password)
   try {
   } catch (error) {
@@ -19,8 +27,43 @@ const validateUser = async (password) => {
   }
 }
 
+const isExistentOAuthAccount = async (profileId, strategy) => {
+  try {
+    const query = await searchOAuthAccountQuery(profileId, strategy)
+    const DBres = await readQuery(query)
+    const acc_id = DBres[0][0]
+    return acc_id
+  } catch (error) {
+    return false
+  }
+}
+const createGoogleAccount = async (profile) => {
+  const accountData = {
+    google_id: profile.id,
+    account_type: 'candidate',
+    accepted_terms: true,
+    email_primary: profile.emails[0].value,
+  }
+  const query = await createAccountWithOAuthQuery(
+    accountData,
+    accountData.google_id,
+    'google_id'
+  )
+
+  try {
+    const res = await readQuery(query)
+
+    return res[0][0]
+  } catch (error) {
+    console.log('Error at createGoogleAccount')
+    return false
+  }
+}
+
 const authUtils = {
   hashPassword,
-  validateUser,
+  validatePassword,
+  isExistentOAuthAccount,
+  createGoogleAccount,
 }
 export default authUtils
