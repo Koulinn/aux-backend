39d9dfd631aff184a46690eb582cb22a5a3da6fa
Rafael Lima <drdverzola@gmail.com>
2022-01-25 09:22:02 +0000

    created validateAvvount and imported account query handlers, lib and authUtils

--- src/server/services/account/utils.js
+++ src/server/services/account/utils.js
@@ -1,3 +1,15 @@
+import accountQueryHandlers from './query_handlers.js'
+import lib from '../../../library/index.js'
+import authUtils from '../../../authentication/auth_utils.js'
+
+const {
+  utils: { readQuery },
+} = lib
+
+const { validatePassword } = authUtils
+
+const { getAccountQuery } = accountQueryHandlers
+
 const setAuthCookie = (res, authToken) => {
   res.cookie('Authorization-token', `Bearer ${authToken}`, {
     maxAge: 900000,
@@ -6,8 +18,32 @@ const setAuthCookie = (res, authToken) => {
   })
 }
 
+const validateAccount = async (email, password) => {
+  try {
+    const query = getAccountQuery('email_primary', email)
+
+    const res = await readQuery(query)
+    if (res[0].length === 0) {
+      return { status: 'notFound', acc_id: null }
+    } else {
+      const { acc_id } = res[0][0]
+      const hashedPassword = res[0][0].password
+
+      const isCorrectPassword = await validatePassword(password, hashedPassword)
+      if (isCorrectPassword) {
+        return { status: 'success', acc_id }
+      } else {
+        return { status: 'wrongPassword', acc_id: null }
+      }
+    }
+  } catch (error) {
+    console.log(error, 'From validateAccount')
+  }
+}
+
 const utils = {
   setAuthCookie,
+  validateAccount,
 }
 
 export default utils
