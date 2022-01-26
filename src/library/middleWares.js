7e38db230d65ea06806e071378ae58c80eb700a8
Rafael Lima <drdverzola@gmail.com>
2022-01-26 08:24:10 +0000

    validate Account middleware

--- src/library/middleWares.js
+++ src/library/middleWares.js
@@ -1,6 +1,9 @@
 import utils from './utils.js'
+import mwQueryHandlers from './query_handlers.js'
 
-const { createError, bodyHasSQLInjection } = utils
+const { createError, bodyHasSQLInjection, readQuery } = utils
+
+const { selectAccountQuery } = mwQueryHandlers
 
 const bodySQLPrevention = (req, res, next) => {
   const isInvalid = bodyHasSQLInjection(req.body)
@@ -11,8 +14,29 @@ const bodySQLPrevention = (req, res, next) => {
   }
 }
 
+const validateAccount = async (req, res, next) => {
+  try {
+    if (!req.acc_id) {
+      next(createError(404, 'Account not found'))
+    } else {
+      const query = selectAccountQuery(req.acc_id)
+      const DB_res = await readQuery(query)
+
+      if (DB_res[0].length === 0) {
+        next(createError(401, 'Account not found'))
+      } else {
+        req.account = DB_res[0][0]
+        next()
+      }
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
 const middleWares = {
   bodySQLPrevention,
+  validateAccount,
 }
 
 export default middleWares
