9ead74a66b83540cb7f53faf8d250dc7c5b9439b
Rafael Lima <drdverzola@gmail.com>
2022-01-28 07:48:06 +0000

    SENDGRID_API_KEY,   AUX_SEO_EMAIL, env

--- src/config/globalVariables/index.js
+++ src/config/globalVariables/index.js
@@ -2,8 +2,16 @@ import dotenv from 'dotenv'
 
 if (process.env.NODE_ENV === 'test') dotenv.config()
 
-const { PORT, APP_MODE, NODE_ENV, DEV_URL, PROD_URL_1, PROD_URL_2 } =
-  process.env
+const {
+  PORT,
+  APP_MODE,
+  NODE_ENV,
+  DEV_URL,
+  PROD_URL_1,
+  PROD_URL_2,
+  SENDGRID_API_KEY,
+  AUX_SEO_EMAIL,
+} = process.env
 
 const globalVariables = {
   PORT,
@@ -12,5 +20,7 @@ const globalVariables = {
   DEV_URL,
   PROD_URL_1,
   PROD_URL_2,
+  SENDGRID_API_KEY,
+  AUX_SEO_EMAIL,
 }
 export default globalVariables
