f004cc44c6fb47960839bbc348cb20de53bd35c7
koulinn <drdverzola@gmail.com>
2022-02-06 12:00:55 +0000

    Export queryHandler from lib

--- src/library/index.js
+++ src/library/index.js
@@ -1,11 +1,13 @@
 import error_handlers from './error_handlers.js'
 import utils from './utils.js'
 import middleWares from './middlewares.js'
+import queryHandlers from './query_handlers.js'
 
 const lib = {
   error_handlers,
   utils,
   middleWares,
+  queryHandlers,
 }
 
 export default lib
